208e1849944b8cbe336b7bc1951e6ccf2975bd9b
Beh LC <behlc2@gmail.com>
2025-11-19 14:52:54 +0800

    Main Menu

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
